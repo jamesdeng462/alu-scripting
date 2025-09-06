e6a3c8088e7aeaf30368aee0e006da84270424f3
jamesdeng462 <j.deng1@alustudent.com>
2025-09-06 13:56:15 -0700

    Add 8-textme.rb - extract from,to,flags from TextMe logs (order-independent)

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,9 +1,18 @@
 #!/usr/bin/env ruby
-# 8-textme.rb
-# Extract [from:...], [to:...], [flags:...] from a log line and print "from,to,flags"
+# 8-textme.rb - extract [from:], [to:] and [flags:] from a log line (order-independent)
 
 text = ARGV[0] || ""
-m = text.match(/\[from:([^\]]+)\].*?\[to:([^\]]+)\].*?\[flags:([^\]]+)\]/)
-if m
-  puts "#{m[1]},#{m[2]},#{m[3]}"
+
+# Find all bracketed key:value pairs like [from:Google] [to:+123] [flags:...]
+pairs = text.scan(/\[([^:\]]+):([^\]]*)\]/)
+
+# Build a map of key => value
+h = {}
+pairs.each do |k, v|
+  h[k] = v
+end
+
+# Print only when all three keys exist
+if h.key?('from') && h.key?('to') && h.key?('flags')
+  puts "#{h['from']},#{h['to']},#{h['flags']}"
 end
